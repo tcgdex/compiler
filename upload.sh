f1f97054fced259ec5ceae80d4c448fe7953a45b
Avior <florian.bouillon@delta-wings.net>
2020-03-17 00:45:37 +0100

    sftp verbose+ ./upload.sh
    Signed-off-by: Avior <florian.bouillon@delta-wings.net>

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sshpass -p $UPLOAD_PASSWORD sftp $UPLOAD_USERNAME@$UPLOAD_REMOTE <<EOF
+sshpass -p $UPLOAD_PASSWORD sftp -vvv $UPLOAD_USERNAME@$UPLOAD_REMOTE <<EOF
 cd net/tcgdex/api
 put -r dist/*
 exit
